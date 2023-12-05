083b832c3fc52e491c6442bd4f868a0e30e65d8b
Anish Shrestha <100459851+shresthanish@users.noreply.github.com>
2023-12-04 22:46:09 -0500

    Add files via upload

---
+++ shrestha-anish_project_dec04/index.js
@@ -0,0 +1,192 @@
+import express from "express";
+// import puppeteer from 'puppeteer';
+import fs from 'fs';
+import path from 'path';
+import pkg from 'pg';
+const { Client } = pkg;
+import bodyParser from 'body-parser';
+
+const port = 3011;
+const app = express();
+app.use(express.static("public/"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+const exportPath = path.join(process.cwd(), 'export');
+
+//database
+const db = new Client({
+  user: "postgres",
+  host: "127.0.0.1", // or 'localhost'
+  database: "consistify",
+  password: "rootpassword",
+  port: 5432
+});
+
+db.connect();
+
+
+// const exportAllPages = async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   // Get all EJS template names
+//   const ejsTemplates = fs.readdirSync('./views').filter(file => file.endsWith('.ejs'));
+
+
+//   // Export each EJS template to static HTML
+//   for (const ejsTemplate of ejsTemplates) {
+//     const htmlTemplateName = ejsTemplate.replace('.ejs', '.html');
+
+//     try {
+//       await page.goto(`http://localhost:${port}/${ejsTemplate.replace('.ejs', '')}`);
+//       const content = await page.content();
+//       const outputPath = path.join(exportPath, htmlTemplateName);
+//       fs.writeFileSync(outputPath, content);
+//       console.log(`Exported ${htmlTemplateName}`);
+//     } catch (error) {
+//       console.error(`Error exporting ${htmlTemplateName}:`, error);
+//     }
+//   }
+
+//   await browser.close();
+// };
+
+// app.get('/export', async (req, res) => {
+//   await exportAllPages();
+//   res.send('HTML export complete.');
+// });
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/newuser", (req, res) => {
+  res.render("newuser.ejs");
+});
+
+
+//new user
+app.post("/addUser", async (req,res)=>{
+  const { firstName, lastName, email, category, age } = req.body;
+  console.log(req.body);
+  try {
+    // Retrieve cat_id from categories table based on the selected category
+    console.log("Before database query");
+    let userId =0;
+    const catresult = await db.query("SELECT cat_id FROM categories WHERE name = $1", [category]);
+    console.log("after database query");
+    const catId = catresult.rows[0]?.cat_id; // Retrieve the first row's cat_id
+    console.log(catId);
+    if (!catId) {
+      console.error("Category not found:", category);
+      return res.status(400).send("Category not found");
+    }
+    const result = await db.query(
+      "INSERT INTO useracc (first_name, last_name, email, category, age) VALUES ($1, $2, $3, $4, $5)",
+      [firstName, lastName, email, catId, age]
+    );
+      //Dec 4th
+      const getUser = await db.query("SELECT id, first_name , last_name ,email FROM useracc WHERE first_name = $1 and last_name = $2 and email=$3", 
+      [firstName, lastName, email]);
+    console.log("after database query");
+    if (getUser.rows.length > 0) {
+      userId = getUser.rows[0].id;
+      console.log("User ID:", userId);
+    
+      // Now you can use the userId as needed
+    } else {
+      console.log("User not found");
+    }
+       res.redirect(`/homepage?userId=${encodeURIComponent(userId)}&firstName=${encodeURIComponent(firstName)}
+       &lastName=${encodeURIComponent(lastName)}&email=${encodeURIComponent(email)}
+       &category=${encodeURIComponent(category)}&age=${encodeURIComponent(age)}`);
+
+ // Redirect to the home page or any other page after successful submission
+  } catch (error) {
+    console.error("Error adding user:", error);
+    res.status(500).send("Internal Server Error");
+  }
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/homepage", (req, res) => {
+  // Access query parameters from the URL
+  console.log(req.query)
+  const { userId,firstName, lastName, email, category, age } = req.query;
+
+  // Render the homepage.ejs template and pass the user details as locals
+  res.render("homepage.ejs", {userId, firstName, lastName, email, category, age });
+});
+
+app.get("/calendar", (req, res) => {
+  // Access query parameters from the URL
+  
+  console.log(req.query+"Calendar");
+  const activityName  = req.query.activity;
+  // Render the homepage.ejs template and pass the user details as locals
+  res.render("calendar.ejs", {activityName});
+});
+
+
+app.get("/existinguser",async (req, res) => {
+  res.render("existinguser.ejs");
+});
+
+app.post("/checkUser",async (req, res) => {
+  const { firstName, lastName, email} = req.body;
+  try {
+    // Check if the user exists in the useracc table
+    const result = await db.query(
+      "SELECT * FROM useracc WHERE first_name = $1 AND last_name = $2 AND email = $3",
+      [firstName, lastName, email]
+    );
+      console.log(result);
+      if (result.rows.length > 0) {
+        const user = result.rows[0]; // Assuming the first row contains user details
+        const { id,category, age } = user;
+  
+        return res.redirect(`/homepage?userId=${encodeURIComponent(id)}&firstName=${encodeURIComponent(firstName)}&lastName=${encodeURIComponent(lastName)}&email=${encodeURIComponent(email)}&category=${encodeURIComponent(category)}&age=${encodeURIComponent(age)}`);
+      }
+  
+      // If user does not exist, handle accordingly (e.g., redirect to a different page)
+      res.send("User not found"); // You can customize this response
+    } catch (error) {
+      console.error("Error checking user:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+
+  //Nov 27th changes
+  // Endpoint to handle updating user activity
+app.post("/updateActivity", async (req, res) => {
+  const { userId, activityId, date, isCompleted } = req.body;
+
+  try {
+    const result = await db.query(
+      "INSERT INTO user_activity (user_id, activity_id, date, is_completed) VALUES ($1, $2, $3, $4) ON CONFLICT (user_id, activity_id, date) DO UPDATE SET is_completed = $4",
+      [userId, activityId, date, isCompleted]
+    );
+
+    console.log("User activity updated successfully:", result.rows);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error updating user activity:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
